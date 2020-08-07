f39ff6fb41c3c9c28c5cc6406e23a9dceb03f5b2
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-08-08 02:31:12 +0300

    Added endpoints for the cards

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
-const { auth,home } = require("../routes");
+const { auth, home, blackCard, whiteCard } = require("../routes");
 
 module.exports = (app) => {
   app.use("/auth", auth);
-  app.use("/homeText",home);
+  app.use("/homeText", home);
+  app.use("/blackCard", blackCard);
+  app.use("/whiteCard", whiteCard);
 };
