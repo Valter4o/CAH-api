67f0f869fb6891c701752f7829ed799ba515574f
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-08-09 22:02:19 +0200

    Added secret to env variables

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || "development";
 const port = process.env.PORT || 6969;
 const dbUrl = `mongodb+srv://user1:${pass}@type-champ-jbbfu.mongodb.net/CAH?retryWrites=true`;
 const authCookie = "x-auth-cookie";
-const secret = "shano";
+const secret = process.env.SECRET;
 
 const config = {
   development: {
