03d4f4a6295b69537ba4e5ac81fcb8d8bd769511
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-08-09 21:38:45 +0200

    cool

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-require("./config/database")().then(() => {
-  const { port } = require("./config/config");
-  const app = require("express")();
-  const appString = `Server is ready, listening on port ${port}`;
+require("./config/database")()
+  .then(() => {
+    const { port } = require("./config/config");
+    const app = require("express")();
+    const appString = `Server is ready, listening on port ${port}`;
 
-  require("./config/express")(app);
-  require("./config/routes")(app);
+    require("./config/express")(app);
+    require("./config/routes")(app);
 
-  app.listen(port, console.log(appString));
-});
-  
+    app.listen(port, console.log(appString));
+  })
+  .catch((err) => console.log(err));
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,10 @@
 const { auth, home, blackCard, whiteCard } = require("../routes");
+const cool = require("cool-ascii-faces");
 
 module.exports = (app) => {
   app.use("/auth", auth);
   app.use("/homeText", home);
   app.use("/blackCard", blackCard);
   app.use("/whiteCard", whiteCard);
+  app.use("/cool", (req, res) => res.send(cool()));
 };
