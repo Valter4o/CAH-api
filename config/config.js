9c0f526466108ceefb9dbace40e5f89274e5a913
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-08-09 21:58:06 +0200

    Removed pass from files

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const pass = "capitan1928374655";
+const pass = process.env.DBPASS;
 const env = process.env.NODE_ENV || "development";
 const port = process.env.PORT || 6969;
 const dbUrl = `mongodb+srv://user1:${pass}@type-champ-jbbfu.mongodb.net/CAH?retryWrites=true`;
