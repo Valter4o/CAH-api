1e3e48b770969c05e8b491bb949c6ef2307b65be
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-08-14 14:56:44 +0200

    Added and removed some props

--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
-const { joinGame, getAll, post } = require("../handlers/game");
+const { getAll, post } = require("../handlers/game");
 
 router.get("/all", getAll);
-router.get("/join/:id", joinGame);
 router.post("/", post);
 
 module.exports = router;
