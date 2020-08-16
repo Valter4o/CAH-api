9aa443e03b157f86d0234000e115d02fcc0d74a5
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-08-16 11:03:35 +0200

    Changed the home text

--- handlers/home.js
+++ handlers/home.js
@@ -1,6 +1,13 @@
 module.exports = function (req, res) {
   res.json({
-    animated: ["Kole", "poluchi li?"],
+    animated: [
+      "Behold the game that no moral and that's cool",
+      "Tip: Don't drink milk while playing this game",
+      "If you see a bug - it's a feature",
+      "Because FUCK everything moral I guess",
+      "We play, bcs we don't care",
+      "Why are you reading this stupid animated text - GO PLAY THE GAME"
+    ],
     static: "static",
   });
 };
