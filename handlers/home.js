59e558931d0f4923ba9aa0519ff9c6d5b131f778
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-08-23 17:07:30 +0300

    Added some more animated texts

--- handlers/home.js
+++ handlers/home.js
@@ -1,12 +1,16 @@
 module.exports = function (req, res) {
   res.json({
     animated: [
-      "Behold the game that no moral and that's cool",
-      "Tip: Don't drink milk while playing this game",
-      "If you see a bug - it's a feature",
       "Because FUCK everything moral I guess",
-      "We play, bcs we don't care",
-      "Why are you reading this stupid animated text - GO PLAY THE GAME"
+      "Warning: This game is not for people with no sense of humor",
+      "This is the game you are looking for *wave*",
+      "We play, because we don't care",
+      "Behold the game that has no moral and that is what makes her cool",
+      "Why are you reading this stupid animated text - GO PLAY THE GAME",
+      "If you see something buggy - it's a feature",
+      "Tip: Don't drink milk while playing this game",
+      "Seriously stop reading this animated text",
+      "You may still wonder why this game is cool ?"
     ],
     static: "static",
   });
